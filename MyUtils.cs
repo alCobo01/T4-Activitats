05cfc465c859d6e7606403b5bb111f34d180bda5
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-03 22:27:33 +0100

    Exercici 6 fet

---
+++ MyUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T4Activitats
+{
+    public static class MyUtils
+    {
+        public static string ReadString()
+        {
+            string input;
+            while (string.IsNullOrEmpty(input = Console.ReadLine()))
+            {
+                Console.Write("Please, write a valid string: ");
+            }
+            return input;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Globalization;
 
 namespace T4Activitats
 {
@@ -7,7 +8,23 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            Console.WriteLine("Introdueix la data en format dd/MM/yyyy (exemple: 25/12/2023):");
+            string input = MyUtils.ReadString();
+
+            DateTime dataObjectiu;
+            //Hem de pasar-li informació sobre la cultura (god knows que significa ??)
+            bool dataValida = DateTime.TryParseExact(input, "dd/MM/yyyy", CultureInfo.InvariantCulture, DateTimeStyles.None, out dataObjectiu);
+
+            if (dataValida)
+            {
+                DateTime avui = DateTime.Today;
+                int diesRestants = (dataObjectiu >= avui) ? (dataObjectiu - avui).Days : 0;
+                Console.WriteLine($"Queden {diesRestants} dies fins a la data indicada.");
+            }
+            else
+            {
+                Console.WriteLine("Error: Format de data incorrecte. Utilitza el format dd/MM/yyyy.");
+            }
         }
     }
 }
