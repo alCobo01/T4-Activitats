a7c7a9a6d13cda2e749afd0c596f6ed7ccdb0b2c
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-03 21:49:18 +0100

    Exercici 5 fet

---
+++ MyUtils.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace T4Activitats
+{
+    public static class MyUtils
+    {
+        public static int ReadInt()
+        {
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Please, write a valid number: ");
+            }
+            return number;
+        }
+        public static string ReadString()
+        {
+            string input;
+            while (string.IsNullOrEmpty(input = Console.ReadLine()))
+            {
+                Console.Write("Please, write a valid string: ");
+            }
+            return input;
+        }
+
+        public static void Display(List<int> list)
+        {
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,32 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            const string FirstMessage = "How many numbers do you want to save? ",
+                 ValueMessage = "Write the number {0} of {1}: ";
+
+            int numberOfValues;
+
+            List<int> values = new List<int>();
+            List<int> evenValues = new List<int>();
+
+            Console.Write(FirstMessage);
+            numberOfValues = MyUtils.ReadInt();
+
+            for (int i = 0; i < numberOfValues; i++)
+            {
+                Console.Write(ValueMessage, i + 1, numberOfValues);
+                values.Add(MyUtils.ReadInt());
+            }
+
+            foreach (int value in values)
+            {
+                if (value % 2 == 0)
+                {
+                    evenValues.Add(value);
+                }
+            }
+
+            MyUtils.Display(evenValues);
         }
     }
 }
