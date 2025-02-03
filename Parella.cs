281b53517a3b6f031e049c3becc28782375cdfe5
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-03 20:17:21 +0100

    Exercici 1 fet

---
+++ Parella.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+
+namespace T4Activitats
+{
+    public class Parella<T1, T2>
+    {
+        public T1 Primer { get; set; }
+        public T2 Segon { get; set; }
+        public Parella(T1 primer, T2 segon)
+        {
+            Primer = primer;
+            Segon = segon;
+        }
+        public override string ToString()
+        {
+            return $"{Primer}, {Segon}";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,11 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            Parella<string, int> parella1 = new Parella<string, int>("Hola", 3);
+            Parella<double, bool> parella2 = new Parella<double, bool>(3.14, true);
+
+            Console.WriteLine(parella1);
+            Console.WriteLine(parella2);
         }
     }
 }
