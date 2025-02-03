20905d0ad02c2dfedfe29022015fcebfb860a04e
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-03 20:35:00 +0100

    Exercici 3 fet

---
+++ MyUtils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace T4Activitats
+{
+    public static class MyUtils
+    {
+        public static int ReadInt()
+        {
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Please, write a valid number: ");
+            }
+            return number;
+        }
+        public static string ReadString()
+        {
+            string input;
+            while (string.IsNullOrEmpty(input = Console.ReadLine()))
+            {
+                Console.Write("Please, write a valid string: ");
+            }
+            return input;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,31 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            const string FirstMessage = "How many students do you want to save? ",
+                NameMessage = "Write the name of the student {0}: ",
+                AgeMessage = "Write the age of the student {0}: ";
+
+            int numberOfValues, age;
+            string name;
+
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+
+            Console.Write(FirstMessage);
+            numberOfValues = MyUtils.ReadInt();
+            for (int i = 0; i < numberOfValues; i++)
+            {
+                Console.Write(NameMessage, i + 1);
+                name = MyUtils.ReadString();
+                Console.Write(AgeMessage, i + 1);
+                age = MyUtils.ReadInt();
+                dictionary.Add(name, age);
+            }
+
+            Console.WriteLine("\nStudents saved:");
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("Name: {0}, Age: {1}", item.Key, item.Value);
+            }
         }
     }
 }
