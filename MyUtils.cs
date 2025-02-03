ec044edc0f584cec3785091dde8dd6c7bf9b3e67
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-03 20:20:15 +0100

    Exercici 2 fet

---
+++ MyUtils.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace T4Activitats
+{
+    public class MyUtils
+    {
+        public static int ReadInt()
+        {
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write("Please, write a valid number: ");
+            }
+            return number;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,25 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            const string FirstMessage = "How many values do you want to write: ",
+                SecondMessage = "Write value {0} of {1}: ";
+
+            int numberOfValues;
+
+            ArrayList array = new ArrayList();
+            Console.Write(FirstMessage);
+            numberOfValues = MyUtils.ReadInt();
+
+            for (int i = 0; i < numberOfValues; i++)
+            {
+                Console.Write(SecondMessage, i + 1, numberOfValues);
+                array.Add(Console.ReadLine());
+            }
+
+            foreach (var item in array)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
