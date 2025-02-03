e818944c4e4e4fc8379980c233cc872db1bbfbfc
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-03 20:15:26 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+
+namespace T4Activitats
+{
+    public class Program
+    {
+        public static void Main()
+        {
+           
+        }
+    }
+}
