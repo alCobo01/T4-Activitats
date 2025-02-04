ee04e636e705a7fd4463628b3f1a3614130950bd
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-04 22:33:47 +0100

    Exercici 7 fet

---
+++ MyUtils.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+namespace T4Activitats
+{
+    public static class MyUtils
+    {
+        public static int ReadInt()
+        {
+            const string invalidNumberMessage = "Please, write a valid number: ";
+
+            int number;
+            while (!int.TryParse(Console.ReadLine(), out number))
+            {
+                Console.Write(invalidNumberMessage);
+            }
+            return number;
+        }
+
+        public static string ReadString()
+        {
+            const string invalidStringMessage = "Please, write a valid string: ";
+
+            string input;
+            while (string.IsNullOrEmpty(input = Console.ReadLine()))
+            {
+                Console.Write(invalidStringMessage);
+            }
+            return input;
+        }
+
+        public static void Display(List<int> list)
+        {
+            foreach (int item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void Display(ArrayList list)
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
+        }
+
+        public static void FindString(ArrayList list, string str)
+        {
+            const string stringFoundMessage = "{0} is in the list";
+            const string stringNotFoundMessage = "{0} is not in the list";
+
+            if (list.Contains(str))
+            {
+                Console.WriteLine(stringFoundMessage, str);
+            }
+            else
+            {
+                Console.WriteLine(stringNotFoundMessage, str);
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -7,7 +7,32 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            ArrayList arrList = ["Maria", "Joan", "Anna", 42, true];
+            MyUtils.Display(arrList);
+
+            arrList.Remove(42);
+            arrList.Remove(true);
+
+            MyUtils.Display(arrList);
+
+            arrList.Insert(2, "Pere");
+
+            MyUtils.FindString(arrList, "Anna");
+
+            //Si sabem que el contingut del arrList es tot string, es pot convertir amb ToArray()
+            //Si no, l'hauriem de fer amb Cast<string>().ToArray()
+            string[] arr = (string[])arrList.ToArray(typeof(string));
+
+            /* 
+            - arrayList.ToArray(typeof(string))
+               - ToArray() és un mètode de ArrayList que retorna un Array amb els elements del ArrayList.
+               - El paràmetre typeof(string) li indica que l'array resultant ha de ser de tipus string[].
+               - Retorna un objecte de tipus Array, però encara no és un string[].
+
+            - (string[]) (Casting)
+                - ToArray(typeof(string)) retorna un objecte de tipus Array, que és una classe base de tots els arrays en C#.
+                - Com sabem que tots els elements del ArrayList són string, fem un casting explícit a string[] perquè el compilador ho tracti correctament.
+             */
         }
     }
 }
