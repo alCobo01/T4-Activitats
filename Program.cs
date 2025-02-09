e5f61ece57f1fe75da88178678db3300054536ad
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-09 16:56:37 +0100

    Exercici 15 fet

--- Program.cs
+++ Program.cs
@@ -7,7 +7,8 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            List<int> list = [13, 25, 87, 52, 21, 90, 5, 8, 10];
+            list.Where(n => n %  2 == 0).ToList().ForEach(n => Console.WriteLine(n));
         }
     }
 }
