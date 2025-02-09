d45633f4a960a135ef02499a1086cc74f1c403e0
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-09 17:06:34 +0100

    Exercici 17 fet

--- Program.cs
+++ Program.cs
@@ -7,7 +7,19 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+           Dictionary<string, float> employees = new Dictionary<string, float>() 
+           {
+               {"John", 1000},
+               {"Mary", 2000},
+               {"Steve", 3000},
+               {"Anna", 4000},
+               {"Peter", 5000}
+           };
+
+            foreach (KeyValuePair<string, float> employee in employees)
+            {
+                Console.WriteLine("Employee: {0}, Salary: {1}", employee.Key, employee.Value);
+            }
         }
     }
 }
