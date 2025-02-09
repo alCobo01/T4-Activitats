f1f31f8d70fa40863e9e0c0d536d769242238c74
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-09 17:17:35 +0100

    Exercici 18 fet

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace T4Activitats
 {
@@ -7,7 +8,15 @@ namespace T4Activitats
     {
         public static void Main()
         {
-            List<int> ints = new List<int> { 1, 2, 3, 4, 5 };
+            const int MAX = 3;
+
+            List<int> list = new List<int> { 1, 2, 3, 4, 5 };
+            list.RemoveAll(n => n > MAX);
+
+            foreach (int n in list)
+            {
+                Console.WriteLine(n);
+            }
         }
     }
 }
