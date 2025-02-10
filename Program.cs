c6bee59bccd55985c6d5c9d8982f5680410ade27
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-10 22:44:08 +0100

    Exercici 20 fet

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections;
+using System.Security.Cryptography.X509Certificates;
 
 namespace T4Activitats
 {
     public class Program
     {
+        public delegate int Operacio(int a, int b);
+        public static int ExecutaOperacio(Operacio op, int a, int b) => op(a, b);
+        public static int Multiplicar(int a, int b) => a * b;
+        public static int Dividir(int a, int b) => a / b;
         public static void Main()
         {
-           
+           Operacio operacio = Multiplicar;
+           int resultat = ExecutaOperacio(operacio, 5, 3);
+           int resultatDos = ExecutaOperacio(Dividir, 10, 2);
         }
     }
 }
