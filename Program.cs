f43a4dfd09ca4a8c4a9821765d69a33c859577b3
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-10 22:37:18 +0100

    Exercici 19 fet

--- Program.cs
+++ Program.cs
@@ -1,13 +1,23 @@
 ﻿using System;
 using System.Collections;
+using System.Security.Cryptography;
 
 namespace T4Activitats
 {
     public class Program
     {
+        public delegate int Operate(int a, int b);
+        public static int Multiplicar(int a, int b) => a * b;
+        public static int Dividir(int a, int b) => a / b;
         public static void Main()
         {
-           
+            Operate multiplicacio = Multiplicar;
+            Operate divisio = Dividir;
+            int resultatMult = multiplicacio(2, 3);
+            int resultatDiv = divisio(6, 2);
+
+            Console.WriteLine(resultatMult);
+            Console.WriteLine(resultatDiv);
         }
     }
 }
