f9f820167c8b1b40f8a735c350bb04b478994865
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-10 22:52:50 +0100

    Exercici 21 fet

--- Program.cs
+++ Program.cs
@@ -5,9 +5,23 @@ namespace T4Activitats
 {
     public class Program
     {
+        public delegate void Notificacio(string missatge);
+        public static void MostrarMissatgeConsola(string missatge)
+        {
+            Console.WriteLine("Missatge: " + missatge);
+        }
+
+        public static void MostrarMissatgeAmbEstreles(string missatge)
+        {
+            Console.WriteLine("********** " + missatge + " **********");
+        }
+
         public static void Main()
         {
-           
+            Notificacio notificacio = MostrarMissatgeConsola;
+            // += agrega un nou mètode al delegat
+            notificacio += MostrarMissatgeAmbEstreles;
+            notificacio("Hola món!");
         }
     }
 }
