996b7f843139921a20a3442ecc0e055bd73a3c6f
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-11 20:43:46 +0100

    Exercici 22 fet

--- Program.cs
+++ Program.cs
@@ -7,7 +7,22 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            Func<int, int, int> multiplicacio = (a, b) => a * b;
+            int result = ExecuteOperation(multiplicacio, 3, 4);
+            Console.WriteLine(result);
+
+            Action<int, int> printSum = (a, b) => Console.WriteLine($"Sum: {a + b}");
+            ExecuteAction(printSum, 3, 4);
+        }
+
+        public static int ExecuteOperation(Func<int, int, int> operation, int x, int y)
+        {
+            return operation(x, y);
+        }
+
+        public static void ExecuteAction(Action<int, int> action, int x, int y)
+        {
+            action(x, y);
         }
     }
 }
