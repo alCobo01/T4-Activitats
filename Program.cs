0c02dedb40359776fd9d171adf8c9ad12ed310da
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-12 10:36:25 +0100

    Exercici 23 fet

--- Program.cs
+++ Program.cs
@@ -5,9 +5,12 @@ namespace T4Activitats
 {
     public class Program
     {
+        public delegate double Operacio(int a, int b); 
+
         public static void Main()
         {
-           
+            Operacio op = (a, b) => Math.Pow(a, b);
+            Console.WriteLine(op(2, 3));
         }
     }
 }
