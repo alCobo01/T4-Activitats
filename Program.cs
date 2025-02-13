008c25823c3642935ade521be608abd25b5d00ab
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-13 13:15:44 +0100

    Exercici 27 fet

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace T4Activitats
 {
     public class Program
     {
+        public static bool IsValidPhoneNumber(string phoneNumber)
+        {
+            return Regex.IsMatch(phoneNumber, @"^(?:\+34\s?)?(\d{9}|\d{3}\s?\d{3}\s?\d{3})$");
+        }
+
         public static void Main()
         {
-           
+            Console.WriteLine(IsValidPhoneNumber("+34 600 123 456"));
+            Console.WriteLine(IsValidPhoneNumber("60012345"));
         }
     }
 }
