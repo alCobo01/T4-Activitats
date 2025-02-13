559d024d740081ab359b80e2feee8c03d8797a8c
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-13 12:53:24 +0100

    Exercici 26 fet

--- Program.cs
+++ Program.cs
@@ -1,13 +1,20 @@
 ﻿using System;
 using System.Collections;
+using System.Text.RegularExpressions;
 
 namespace T4Activitats
 {
     public class Program
     {
+        public static bool IsValidEmail(string email)
+        {
+            //explicació regex: https://chatgpt.com/share/67addd07-ead4-800d-9185-b1d6786daccb
+            return Regex.IsMatch(email, @"^([\w\.\-]+)@([\w\-]+)((\.(\w){2,3})+)$");
+        }
         public static void Main()
         {
-           
+            Console.WriteLine(IsValidEmail("usuari@gmail.com"));
+            Console.WriteLine(IsValidEmail("usuari@gmail"));
         }
     }
 }
