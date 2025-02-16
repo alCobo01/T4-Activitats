ce3ac6bc8b50ea85d76119cdd898ab9bbb11ff3a
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-16 22:49:42 +0100

    Exercici 32 fet

--- Program.cs
+++ Program.cs
@@ -7,7 +7,26 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            string[] alumnes =
+            [
+            "Joan Garcia, 9.5",
+            "Anna Lopez, 8.0",
+            "Marc Puig, 7.5",
+            "Laura Costa, 9.0",
+            "Pau Marti, 8.5"
+            ];
+
+            string path = Path.GetFullPath(@"..\..\..\files\text.txt");
+
+            File.WriteAllLines(path, alumnes);
+
+            using StreamReader sr = File.OpenText(path);
+            string? line;
+            while ((line = sr.ReadLine()) != null)
+            {
+                Console.WriteLine(line);
+            }
+
         }
     }
 }
