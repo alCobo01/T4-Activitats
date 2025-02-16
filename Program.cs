ab087cd870907de92bd26661e0e45e0745fa3ecf
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-16 23:21:29 +0100

    Exercici 33 començat

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
-using System.Collections;
+using System.Globalization;
+using CsvHelper;
 
 namespace T4Activitats
 {
@@ -7,7 +8,14 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            using var reader = new StreamReader("students.csv");
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csv.GetRecords<Student>();
+            foreach (var student in records)
+            {
+                Console.WriteLine($"Nom: {student.Nom}, Edat: {student.Edat}, Ciutat: {student.Ciutat}");
+            }
+
         }
     }
 }
---
+++ Students.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace T4Activitats
+{
+    public class Student
+    {
+        public int Edat { get; set; }
+        public string Nom { get; set; }
+        public string Ciutat { get; set; }
+    }
+}
