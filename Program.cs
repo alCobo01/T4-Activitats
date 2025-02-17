dc5e45976668d42cc292c801c7f0e6c5c4d365b4
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-17 13:04:41 +0100

    Exercici 31 cmençat

--- Program.cs
+++ Program.cs
@@ -7,7 +7,29 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            //Font de dades
+            List<int> list = new List<int> { 1, -1, 59, 34, 21, -22};
+
+            //Apartat A
+            var evenQuery = from num in list
+                            where (num % 2) == 0
+                            select num;
+
+            //Apartat B
+            var positiveQuery = from num in list
+                                where num > 0
+                                select num;
+
+            //Apartat C
+            var greatherThanTwentyQuery = from num in list
+                                          where num > 20
+                                          select num;
+
+            //Apartat D
+
+
+
+
         }
     }
 }
