670f91d127f308a38ba73b996a2ab90ede71996e
Alvaro Cobo <alvaro.cobo.7e6@itb.cat>
2025-02-17 12:55:16 +0100

    Exercici 35 fet

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using System.Text.Json;
 
 namespace T4Activitats
 {
@@ -7,7 +8,23 @@ namespace T4Activitats
     {
         public static void Main()
         {
-           
+            const string Message = "Fitxer creat correctament! Es pot consultar a {0}";
+
+            string path = Path.GetFullPath(@"..\..\..\files\llibres.json");
+            System.IO.Directory.CreateDirectory(@"..\..\..\files\");
+
+            List<Llibre> llistaLlibres = new List<Llibre>
+            {
+                new Llibre{Any=1999, Autor= "J.K. Rowling", Titol="Harry Potter"},
+                new Llibre{Any=2003, Autor= "Dan Brown", Titol="El Codigo Da Vinci"},
+                new Llibre{Any=2010, Autor= "Suzanne Collins", Titol="Los juegos del hambre"},
+                new Llibre{Any=2012, Autor= "E.L. James", Titol="50 sombras de Grey"}
+            };
+
+            string jsonString = JsonSerializer.Serialize(llistaLlibres);
+            File.WriteAllText(path, jsonString);
+
+            Console.WriteLine(Message, path);
         }
     }
 }
